19b6500f50d32a27258892107764c0339696b2d5
Bram Cohen <bram@chia.net>
2022-12-18 00:44:47 -0800

    Added a hack for alpha blending by making it bigger and downsizing. Also added circles to the end of line segments to make their joints not look weird.

--- fill.py
+++ fill.py
@@ -1,17 +1,25 @@
 from PIL import Image, ImageDraw
 import imageio
 
-def drawone(window, centerx, centery, boxsize, steps, stepnum, m):
-    delta = (1-m*2)*(boxsize/2-boxsize*stepnum/steps)
-    window.line((int(centerx-delta), int(centery-boxsize/2), int(centerx+delta), int(centery+boxsize/2)), fill=(0, 0, 0), width=3)
-    window.line((int(centerx-boxsize/2), int(centery+delta), int(centerx+boxsize/2), int(centery-delta)), fill=(0, 0, 0), width=3)
+def drawend(window, x, y, size):
+    size = size/2 - 1
+    window.ellipse((int(x-size), int(y-size), int(x+size), int(y+size)), fill=(0, 0, 0), width=0)
 
-def draweverything(window, centerx, centery, boxsize, numboxes, steps, stepnum):
+def drawone(window, centerx, centery, boxsize, steps, stepnum, m, factor):
+    delta = (1-m*2)*(boxsize/2-boxsize*stepnum/steps)
+    width = 3*factor
+    window.line((int(centerx-delta), int(centery-boxsize/2), int(centerx+delta), int(centery+boxsize/2)), fill=(0, 0, 0), width=width)
+    drawend(window, centerx-delta, centery-boxsize/2, width)
+    window.line((int(centerx-boxsize/2), int(centery+delta), int(centerx+boxsize/2), int(centery-delta)), fill=(0, 0, 0), width=width)
+    drawend(window, centerx-boxsize/2, centery+delta, width)
+
+def draweverything(window, centerx, centery, boxsize, numboxes, steps, stepnum, factor):
     for i in range(numboxes):
         for j in range(numboxes):
-            drawone(window, centerx+i*boxsize, centery+j*boxsize, boxsize, steps, stepnum, (i&1)^(j&1))
+            drawone(window, centerx+i*boxsize, centery+j*boxsize, boxsize, steps, stepnum, (i&1)^(j&1), factor)
 
-def makeone(boxsize, numboxes, numsteps):
+def makeone(boxsize, numboxes, numsteps, factor):
+    boxsize *= factor
     images = []
     def ni():
         img = Image.new('RGB', (numboxes*boxsize, numboxes*boxsize), (255,255,255))
@@ -19,16 +27,17 @@ def makeone(boxsize, numboxes, numsteps):
         return (img, ImageDraw.Draw(img))
     for i in range(numsteps):
         img, w = ni()
-        draweverything(w, boxsize/2, boxsize/2, boxsize, numboxes, numsteps, i)
+        draweverything(w, boxsize/2, boxsize/2, boxsize, numboxes, numsteps, i, factor)
     for i in range(numsteps):
         img, w = ni()
-        draweverything(w, 0, 0, boxsize, numboxes+1, numsteps, i)
+        draweverything(w, 0, 0, boxsize, numboxes+1, numsteps, i, factor)
     for i in range(numsteps):
         img, w = ni()
-        draweverything(w, boxsize/2, -boxsize/2, boxsize, numboxes+1, numsteps, i)
+        draweverything(w, boxsize/2, -boxsize/2, boxsize, numboxes+1, numsteps, i, factor)
     for i in range(numsteps):
         img, w = ni()
-        draweverything(w, 0, -boxsize, boxsize, numboxes+2, numsteps, i)
-    imageio.mimsave('movie.mp4', images)
+        draweverything(w, 0, -boxsize, boxsize, numboxes+2, numsteps, i, factor)
+    images = [im.resize((numboxes*boxsize//factor, numboxes*boxsize//factor)) for im in images]
+    imageio.mimsave('movie.gif', images)
 
-makeone(50, 10, 40)
+makeone(50, 10, 40, 16)
